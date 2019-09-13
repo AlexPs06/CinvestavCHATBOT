c8a892773314a52d02645501eafe081889feb0ca
AlexPs06 <luis_pesar@hotmail.com>
2019-09-13 12:33:02 -0500

    corregi pruebas

--- src/app/xyz/xyz.component.spec.ts
+++ src/app/xyz/xyz.component.spec.ts
@@ -1,5 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material';
+const angularMaterial=[
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatInputModule,
+  MatIconModule
+]
+
 import { XyzComponent } from './xyz.component';
 
 describe('XyzComponent', () => {
@@ -8,6 +21,9 @@ describe('XyzComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        angularMaterial
+      ],
       declarations: [ XyzComponent ]
     })
     .compileComponents();
