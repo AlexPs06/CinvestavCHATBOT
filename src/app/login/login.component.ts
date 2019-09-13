85416c69ae7a7f215a9ed05682372bc7b49f75df
AlexPs06 <luis_pesar@hotmail.com>
2019-09-13 12:20:05 -0500

    añadi el maquetado del login sin logica solo vista

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  formLogin : FormGroup;
+  submit=false;
+
+  constructor(
+    private formbuilder : FormBuilder,
+    private router: Router
+
+
+  ) { 
+    this.formLogin = this.formbuilder.group({
+      username : [''],
+      password : ['']
+    })
+  }
+
+  ngOnInit() {
+  }
+  comprobarLogin(){
+    
+  }
+  sendLogin(){
+    this.submit=true;
+    if(this.formLogin.invalid){
+      console.log("error contraseña o usuarion no validos")
+     // this.changeSuccessMessage1()
+      return;
+    }
+
+    this.comprobarLogin();
+    this.submit=false;
+
+    console.log(this.formLogin.value.username+"--"+this.formLogin.value.password)
+    
+  }
+
+}
