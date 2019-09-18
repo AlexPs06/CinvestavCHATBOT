957c6ddd567a3f8bbe7d4666e9fb75138508fe81
AlexPs06 <luis_pesar@hotmail.com>
2019-09-18 18:01:26 -0500

    movi todos los componentes a una carpeta en especifico para ellos components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { XyzComponent } from './xyz/xyz.component';
-import { AbcComponent } from './abc/abc.component';
+import { XyzComponent } from '../components/xyz/xyz.component';
+import { AbcComponent } from '../components/abc/abc.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from '../components/login/login.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from '../components/header/header.component';
 import {MatInputModule} from '@angular/material';
-import { RegisterComponent } from './register/register.component';
+import { RegisterComponent } from '../components/register/register.component';
 import {MatRadioModule} from '@angular/material/radio';
-import { MenuComponent } from './menu/menu.component';
-import { ChatbotComponent } from './chatbot/chatbot.component';
+import { MenuComponent } from '../components/menu/menu.component';
+import { ChatbotComponent } from '../components/chatbot/chatbot.component';
 const angularMaterial=[
   MatCardModule,
   MatButtonModule,
