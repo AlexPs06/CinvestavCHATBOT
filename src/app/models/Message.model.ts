18e65c850a0cd9a601b45f8020163943d79409e4
AlexPs06 <luis_pesar@hotmail.com>
2019-10-22 13:08:52 -0500

    modifique el modelo de los mensajes

--- src/app/models/Message.model.ts
+++ src/app/models/Message.model.ts
@@ -1,5 +1,19 @@
 export class Message {
-    
+    public dateCondition=false;
+    public dateActivate=false;
     constructor(public content: String, public idUser: String, public name:String) {}
+
+    /**
+     * metodo para obtener el valor de la condicion del calendario
+     */
+    public getDateCondition() {
+      return this.dateCondition;
+    }
+    /**
+     * metodo para cambiar el valor de la condicion del calendario
+     */
+    public setDateCondition(data:boolean){
+      this.dateCondition=data;
+    }
   }
   
